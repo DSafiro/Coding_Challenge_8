4f79a720d8c03ada201c3efebcd70ac0dc454fc6
Dylan <safirodylan@gmail.com>
2025-02-16 14:28:05 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -6,3 +6,12 @@ function calculateSalary (baseSalary, bonus, taxRate) {
 }; // Declares a function that calculates salary
 calculateSalary(5000, 500, 0.1); // Inputs values and produces expected output of $5000.00
 calculateSalary(7000, 1000, 0.15); // Inputs values and produces expected output of $6950.00
+
+// Task 2: Product Price After Discount
+function calculateDiscount (price, discountRate) {
+    let finalPrice = price - (price * discountRate); // Calculates final price by subtracting price from price * discount rate
+    console.log(`Final Price: $${finalPrice.toFixed(2)}`); // Outputs and rounds final price
+    return finalPrice; // Returns final price
+}; // Declares function that calulates final price after discount
+calculateDiscount(100, 0.2); // Inputs values and produces expected output of $80.00
+calculateDiscount(250, 0.15); // Inputs values and produces expected output of $212.50
